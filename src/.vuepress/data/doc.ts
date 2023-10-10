d3c0ceee9441964d8a76c8dcba22f527b4e5bd61
Mr.Box <1796535192@qq.com>
2023-10-10 18:31:26 +0800

    添加仓库

--- src/.vuepress/data/doc.ts
+++ src/.vuepress/data/doc.ts
@@ -14,6 +14,18 @@ export const doc_front: LinkData[] = [
     ico: "https://element.eleme.cn/favicon.ico",
     link: "https://element.eleme.cn/#/zh-CN",
   },
+  {
+    name: "Layui",
+    desc: "原生态 · 开源极简模块化 Web UI 组件库",
+    ico: "https://layui.dev/favicon.ico",
+    link: "https://layui.dev/",
+  },
+  {
+    name: "uni-app",
+    desc: "开发所有前端应用的框架，一套代码多平台使用",
+    ico: "https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/icon.png",
+    link: "https://uniapp.dcloud.net.cn/",
+  },
 ];
 
 
@@ -30,6 +42,18 @@ export const doc_after: LinkData[] = [
     ico: "https://redis.io/images/favicons/favicon-32x32.png",
     link: "https://redis.io/",
   },
+  {
+    name: "RuoYi",
+    desc: "可以用于所有的Web应用程序，如网站管理后台，网站会员中心，CMS，CRM，OA",
+    ico: "http://doc.ruoyi.vip/images/favicon.ico",
+    link: "http://www.ruoyi.vip/",
+  },
+  {
+    name: "Hutool",
+    desc: "小而全的Java工具类库，通过静态方法封装，降低相关API的学习成本，提高工作效率。",
+    ico: "https://www.hutool.cn/favicon.ico",
+    link: "https://www.hutool.cn/",
+  },
 ];
 
 export const doc_IoT: LinkData[] = [
@@ -38,5 +62,11 @@ export const doc_IoT: LinkData[] = [
     desc: "开发板的技术交流平台，各大半导体厂商官方开发板的支持，电子爱好者的自主设计开发板 ",
     ico: "https://www.yiboard.com/favicon.ico",
     link: "https://www.yiboard.com/forum.php/",
+  },
+  {
+    name: "巴法云",
+    desc: "巴法云物联网平台采用前后端分离的设计思想，致力于攻克高性能的异步并发的物联网端服务器",
+    ico: "https://cloud.bemfa.com/img/favicon.ico",
+    link: "https://cloud.bemfa.com/",
   },
 ];
