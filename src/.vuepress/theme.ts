363460520f7e5158b2be5d7e4e4a1381b3a6440c
Mr.Box <1796535192@qq.com>
2024-01-03 09:36:10 +0800

    修改评论区域名

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -90,7 +90,7 @@ export default hopeTheme({
 
     comment: {                  // 评论服务配置
       provider: "Waline",
-      serverURL: "https://waline.cailei.site/", /* https://waline-comment.vuejs.press 官方自己的地址，放着备用*/
+      serverURL: "https://waline.cailei.fun/", /* https://waline-comment.vuejs.press 官方自己的地址，放着备用*/
       copyright: false,                          // 是否显示页脚Waline的版权信息(Powered by Waline v2.15.7)
       reaction: true,                            // 开启反应（你认为这篇文章怎么样？）
     },
